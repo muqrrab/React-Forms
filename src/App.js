b004ec259422aa63d1d989750ca6dc982467d638
Muqrrab <muqrrab2@gmail.com>
2022-07-15 22:44:04 +0500

    React_forms

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+function App() {
+  return (
+    <div className="container">
+      <h2>My react App</h2>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import ControlledInputs from "./form-multiple-inputs";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ControlledInputs />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
